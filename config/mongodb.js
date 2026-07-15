1d21dd99924596d514beb962fcf00fab8af71d49
Kaviyaa <kaviyaalordminds.com>
2026-07-15 20:37:06 +0530

    corrections

--- config/mongodb.js
+++ config/mongodb.js
@@ -5,9 +5,19 @@ const connectDB = async ()=>{
     mongoose.connection.on("connected", ()=>{
         console.log("Database connected");
     })
-    await mongoose.connect(process.env.MONGODB_URI, {
-        
+
+    mongoose.connection.on("error", (err) => {
+        console.error("MongoDB connection error:", err.message);
     })
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            serverSelectionTimeoutMS: 10000,
+        })
+    } catch (err) {
+        console.error("Failed to connect to MongoDB:", err.message);
+        console.error("Server will continue running, but DB-dependent routes will fail until this is fixed.");
+    }
 }
 
 export default connectDB
--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+import dns from "dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import "./config/env.js";
 import express from "express";
 import cors from "cors";
